b1a09f2ad41fc125fbf04b1eb96af9ed32ba4af0
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2015-03-04 18:02:20 +0700

    chương 6

--- app/views.py
+++ app/views.py
@@ -1,30 +1,85 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, lm, db, oid
 from .forms import LoginForm
+from .models import User, Post
+
 
 @app.route('/')
 @app.route('/index')
-
+@login_required
 def index():
-	user = {'nickname':'nxcong'}	
-	posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        },
-        { 
-            'author': {'nickname': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        }
+    user = g.user
+    posts = [  # fake array of posts
+               {
+                   'author': {'nickname': 'John'},
+                   'body': 'Beautiful day in Portland!'
+               },
+               {
+                   'author': {'nickname': 'Susan'},
+                   'body': 'The Avengers movie was so cool!'
+               }
     ]
 
-	return render_template('index.html',title='Home',user=user,posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login',methods=['GET','POST'])
 
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for OpenID="%s", remember_me=%s'%(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
-	return render_template('login.html',title='Sign In',form=form,providers=app.config['OPENID_PROVIDERS'])
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@oid.after_login
+def after_login(response):
+    if response.email is None or response.email == "":
+        flash('Invalid Login.Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=response.email).first()
+    if user is None:
+        nickname = response.nickname
+        if nickname is None or nickname == "":
+            nickname = response.email.split('@')[0]
+        user = User(nickname=nickname, email=response.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop(remember_me,'None')
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    _user = User.query.filter_by(nickname=nickname).first()
+    if _user is None:
+        flash('User %s not found ' % nickname)
+        return redirect(url_for('index'))
+    posts = Post.query.filter_by(user_id=_user.id)
+    if posts is None:
+        posts = [
+            {'author': _user, 'body': 'Test post #1'},
+            {'author': _user, 'body': 'Test post #2'}
+        ]
+    return render_template('user.html', user=_user, posts=posts)
